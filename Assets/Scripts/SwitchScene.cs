6ad8fa71f2d5129ba00a0b25ccfed500f4c75381
GlenLehrer <300085063@fvtc.edu>
2024-12-08 12:15:09 -0600

    Fixed Menus
    Menus switch between other menus efficiently.  Script was also changed.

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -7,4 +7,63 @@ public class SwitchScene : MonoBehaviour
     {
         SceneManager.LoadScene(scene);
     }
+    public void LoadMenu(string scene)
+    {
+        GameObject CanvasParent = GameObject.Find("CanvasParent");
+
+        GameObject MainMenu = CanvasParent.transform.Find("Main Menu").gameObject;
+        GameObject CreateAccount = CanvasParent.transform.Find("Create Account").gameObject;
+        GameObject Login = CanvasParent.transform.Find("Login").gameObject;
+        GameObject GameRoom = CanvasParent.transform.Find("Game Room").gameObject;
+
+        if (scene == "MainMenu")
+        {
+            MainMenu.gameObject.SetActive(true);
+
+            CreateAccount.gameObject.SetActive(false);
+            Login.gameObject.SetActive(false);
+            GameRoom.gameObject.SetActive(false);
+        }
+        else if (scene == "CreateAccount")
+        {
+            CreateAccount.gameObject.SetActive(true);
+
+            MainMenu.gameObject.SetActive(false);
+            Login.gameObject.SetActive(false);
+            GameRoom.gameObject.SetActive(false);
+        }
+        else if (scene == "Login")
+        {
+            Login.gameObject.SetActive(true);
+
+            MainMenu.gameObject.SetActive(false);
+            CreateAccount.gameObject.SetActive(false);
+            GameRoom.gameObject.SetActive(false);
+        }
+        else if (scene == "GameRoom")
+        {
+
+            GameRoom.gameObject.SetActive(true);
+
+            MainMenu.gameObject.SetActive(false);
+            CreateAccount.gameObject.SetActive(false);
+            Login.gameObject.SetActive(false);
+
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    //GameObject loaded
+    void Start()
+    {
+
+    }
+
+    //Everytime frames are updated
+    void Update()
+    {
+    }
 }
